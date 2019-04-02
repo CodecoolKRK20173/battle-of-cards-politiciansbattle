61551268f168c84dd5d91c932350a8fc4d91615b
Patryk Topolski <venirro@gmail.com>
2019-04-02 16:26:20 +0200

    card

---
+++ battleOfCards/src/main/java/com/codecool/battleofcards/Card.java
@@ -0,0 +1,57 @@
+package main.java.com.codecool.battleofcards;
+
+
+import java.util.Comparator;
+
+public class Card{
+    int support;
+    int money;
+    int bribes;
+
+    Card(int support, int money, int bribes){
+        this.support = support;
+        this.money = money;
+        this.bribes = bribes;
+    }
+
+    public int getBribes() {
+        return bribes;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public int getSupport() {
+        return support;
+    }
+
+
+}
+
+class sortBySupport implements Comparator<Card>{
+    @Override
+    public int compare(Card card1, Card card2) {
+
+        return card1.support - card2.support;
+    }
+}
+
+class sortByMoney implements Comparator<Card>{
+    @Override
+    public int compare(Card card1, Card card2) {
+
+        return card1.money - card2.money;
+    }
+}
+
+class sortByBribes implements Comparator<Card>{
+    @Override
+    public int compare(Card card1, Card card2) {
+
+        return card1.bribes - card2.bribes;
+    }
+}
+
+
+
