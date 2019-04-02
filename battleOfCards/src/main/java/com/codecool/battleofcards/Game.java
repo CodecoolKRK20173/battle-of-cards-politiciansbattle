3c69737b65a172d82956ee1823dfc1a7836e902d
Patryk Topolski <venirro@gmail.com>
2019-04-02 17:34:50 +0200

    sceleton Game class implemented

---
+++ battleOfCards/src/main/java/com/codecool/battleofcards/Game.java
@@ -0,0 +1,24 @@
+package main.java.com.codecool.battleofcards;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Game {
+    private Pile table;
+    private List<Player> players;
+
+    public Game(int players) {
+        this.table = new Pile(Pile.PileType.table);
+        this.players = new ArrayList<>();
+        setPlayers(players);
+    }
+
+
+    private void setPlayers(int players){
+        for (int i = 0; i < players; i++) {
+            this.players.add(new Player());
+        }
+    }
+
+}
