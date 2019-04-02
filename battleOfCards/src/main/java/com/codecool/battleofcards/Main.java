f31864b280809ed3144b30613932b386fcbdc14f
Patryk Topolski <venirro@gmail.com>
2019-04-02 14:16:37 +0200

    initial commit

---
+++ battleOfCards/src/main/java/com/codecool/battleofcards/Main.java
@@ -0,0 +1,13 @@
+package main.java.com.codecool.battlefcards;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
