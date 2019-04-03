ae21c2b3494e45f172e06d374dbb80e8a6727977
Patryk Topolski <venirro@gmail.com>
2019-04-03 13:47:41 +0200

    implemented check who is the winner

--- battleOfCards/src/main/java/com/codecool/battleofcards/Game.java
+++ battleOfCards/src/main/java/com/codecool/battleofcards/Game.java
@@ -1,8 +1,10 @@
 package com.codecool.battleofcards;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
+import java.util.ArrayList;
 import com.codecool.battleofcards.Card.Cards;
 
 
@@ -34,10 +36,26 @@ public class Game {
         return table;
     }
 
-    // private void dealCards(){
-    //     for (int i = 0; i < 6 ; i++) {
-    //         table.addCard(card);
-    //     }
-    // }
+    public Card getWiningCard(String demand) throws IOException{
+        ArrayList<Card> playerTopCards = getPlayersTopCards();
+        for (Player player : players) {
+            if(demand == "bribe"){
+                Collections.sort(playerTopCards, new sortByBribes());
+            }else if(demand == "support"){
+                Collections.sort(playerTopCards, new sortBySupport());
+            }else if(demand == "money"){
+                Collections.sort(playerTopCards, new sortByMoney());
+            }else throw new IOException("sorry somthing went wrong");
+        }
+        return playerTopCards.get(-1);
+    }
+
+    private ArrayList<Card> getPlayersTopCards(){
+        ArrayList<Card> playerTopCards = new ArrayList<>();
+        for (Player player : players) {
+            playerTopCards.add(player.getStack().getTopCard());
+        }
+        return playerTopCards;
+    }
 
 }
