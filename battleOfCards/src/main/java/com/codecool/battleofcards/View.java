ff1565f676c8408a1134673624b7977173c8ed23
Patryk Topolski <venirro@gmail.com>
2019-04-04 15:16:17 +0200

    view

--- battleOfCards/src/main/java/com/codecool/battleofcards/View.java
+++ battleOfCards/src/main/java/com/codecool/battleofcards/View.java
@@ -3,11 +3,13 @@ package com.codecool.battleofcards;
 import java.util.Scanner;
 
 
+
+
 public class View{
     public static String chooseValueToCompare(){
         System.out.println("which value you want to compare? Bribes, Money or Support");
         Scanner sc = new Scanner(System.in);
-        String answer = sc.nextLine();
+        String answer = sc.nextLine().trim();
         if (answer.equalsIgnoreCase("bribes") || answer.equalsIgnoreCase("money")
         || answer.equalsIgnoreCase("support")){
             return answer;
@@ -17,5 +19,26 @@ public class View{
         }
         return "";
 
+    }
+
+    public static String getAnswer(){
+        System.out.println("do you want play again?");
+        Scanner sc = new Scanner(System.in);
+        String answer = sc.nextLine();
+        return answer;
+    }
+
+    public static void makeAPause(Integer miliseconds){
+        try{
+            Thread.sleep(miliseconds);
+        }
+        catch(InterruptedException ex){
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
